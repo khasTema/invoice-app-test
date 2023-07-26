273b70635d0f8b088eb60fcbebc35fea702d5df6
Artem Khasin <91724948+khasTema@users.noreply.github.com>
2023-07-26 20:12:09 +0200

    upt

--- src/components/InvoiceView/InvoiceItem/InvoiceItem.tsx
+++ src/components/InvoiceView/InvoiceItem/InvoiceItem.tsx
@@ -10,8 +10,8 @@ export const InvoiceItem:React.FC<ItemProps> = ({item}):JSX.Element => {
     <div className='items-head w-full flex justify-end items-center text-sm mb-6'>
         <span className='mr-auto'>{name}</span>
         <span className='w-2/12 text-center'>{quantity}</span>
-        <span className='w-2/12 text-center'>{CURRENCY_SYMBOL} {price.toFixed(2)}</span>
-        <span className='w-2/12 text-right font-bold'>{CURRENCY_SYMBOL} {total.toFixed(2)}</span>
+        <span className='w-2/12 text-center'>{CURRENCY_SYMBOL} {price}</span>
+        <span className='w-2/12 text-right font-bold'>{CURRENCY_SYMBOL} {total}</span>
     </div>
   )
 }
